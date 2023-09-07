b60e0e16b984da4dfb9ecedacb431239a9f820b7
PerezDeral365 <124410553+Deral365@users.noreply.github.com>
2023-09-06 19:17:26 -0700

    Practica 3 06/09/23

--- Parcial1/Practica3.js
+++ Parcial1/Practica3.js
@@ -1,7 +1,34 @@
 $(document).ready(function (){
-     var contador=0;
+  var carta1="";var carta2="";
+  var par=false;
+  var total_pares=0;
           $('img').click(function(e){
+            var estado=$(this).attr('data-estado')
             var nombre_img=$(this).attr('data-id')
-            $(this).attr('src','imagenes/'+nombre_img);
+           
+            if (estado=="0"){
+              console.log("Estado de la carta: " + estado)
+              if (carta1 ==""){
+                 carta1=$(this);
+                 carta1.attr('src','imagenes/'+nombre_img)
+                 console.log("Se asigno carta #1")
+              }else{
+                carta2=$(this);
+                carta2.attr('src','imagenes/'+ nombre_img)
+                console.log("Se asigno carta #2")
+              }
+            }
+            
+
+            if(carta1 !="" && carta2!=""){
+              if(carta1.attr('data-id')==carta2.attr('data-id')){
+                par = true
+                total_pares++
+                $("#total_pares").html(total_pares)
+                carta1=""
+                carta2=""
+              }
+            }
+          
           });
 });
