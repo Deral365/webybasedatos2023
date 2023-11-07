bf799bfbf603348c300299ae16b669c90282d882
PerezDeral365 <124410553+Deral365@users.noreply.github.com>
2023-11-06 19:28:26 -0700

    06/11/2023

--- Parcial 2/menu.php
+++ Parcial 2/menu.php
@@ -20,6 +20,17 @@
             <li><a class="dropdown-item" href="RegistrarAlumno.php">Registar alumno</a></li>
           </ul>
         </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Materias
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="inicio.php">Inicio</a></li>
+            <li><a class="dropdown-item" href="consultarAlumnos.php">Consultar _materias</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="RegistrarAlumno.php">Regitrar Materias</a></li>
+          </ul>
+        </li>
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
